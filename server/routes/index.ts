8dcdd05f2396876ba056eecb3f2e85f5bdcb3209
CuongNguyen291201 <cuongnguyen291201@gmail.com>
2021-10-27 23:35:50 +0700

    fix: conflict

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,16 +1,10 @@
 import { Router } from "express";
 import { eventRouters } from "./event";
-<<<<<<< HEAD
 import { userRouters } from "./user";
-=======
->>>>>>> main
 
 const router = Router();
 
 router.use(eventRouters);
-<<<<<<< HEAD
 router.use(userRouters)
-=======
->>>>>>> main
 
 export { router as apiRouters };
